b14562ffd75df0b98356e9a9e6d8bba9832f7fcc
baloght1 <112930774+baloght1@users.noreply.github.com>
2022-10-04 09:39:21 +0200

    Add files via upload

---
+++ 9_10_ora.py
@@ -0,0 +1,26 @@
+""" #Első feladat 9dk tankönyv 
+IDEI_ÉV = 2022
+print(type)(2022))
+print('Az idei év:',2022,sep='--->')
+felhasználó_kora = input('hány éves vagy?')
+print(type(felhasználó_kora))
+print('te most',felhasználó_kora,'15 éves vagy.')
+print (type)) felhasználó_kora = int (felhasználó_kora) 
+születési_év = 2022 - felhasználó_kora
+print('Ekkor születtél:', születési_év,'.', sep='') """  
+
+
+#masodik prog
+
+gondolt_szám = 4
+tipp = input('Gondoltam egy szmára. Tippled meg!')
+tipp = int (tipp)
+if tipp == gondolt_szám:
+    print('Ügyes!')
+    print('Gratulálok')
+if tipp >= gondolt_szám:
+    print('nagyobb')
+if tipp <= gondolt_szám
+    print (kisebb)
+if tipp == gondolt_szám
+    print ('egyenlő')
