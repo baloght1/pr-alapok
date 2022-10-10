3cbc2c2e36b7b4aa2509a803d371ff0d281bbab3
baloght1 <112930774+baloght1@users.noreply.github.com>
2022-10-10 23:27:40 +0200

    Add files via upload

---
+++ prhf.py
@@ -0,0 +1,16 @@
+print ("Kérem az első számot 1-100 között:") 
+
+x = 34
+print (x)
+
+
+print ("Kérem az második számot 1-100 között:") 
+
+y = 23
+print (y)
+
+print (x+y)
+print (x-y)
+print (x*y)
+
+
