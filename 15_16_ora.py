aa6a812d8efca8dda7078eb7a2adafbf6c60af4e
baloght1 <112930774+baloght1@users.noreply.github.com>
2022-10-25 09:38:57 +0200

    Add files via upload

---
+++ 15_16_ora.py
@@ -0,0 +1,40 @@
+'''
+print("Hello")
+
+print('Hello')
+
+a = 'Hello'
+
+print(a)
+a =Lorem ipsum dolor sit amet,
+
+consectetur adipiscing elit,
+
+sed do eiusmod tempor incididunt
+
+ut labore et dolore magna aliqua.
+
+print(a)
+
+
+a = "Hello, World!"
+
+print(a[0])
+
+print(a[4])
+for x in "banana":
+
+  print(x)
+
+for x in "banana":
+
+  print(x,end='')
+'''
+
+a = "Hello, World!"
+
+szamlalo = 1
+for x in a:
+    if szamlalo % 2 == 0:
+        print(a[szamlalo-1], end="")
+     szamlalo = szamlalo + 1
