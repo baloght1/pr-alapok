4a895502516387ecdc8da5fbf6fd7b41cced107b
baloght1 <112930774+baloght1@users.noreply.github.com>
2022-11-08 09:40:47 +0100

    Add files via upload

--- 15_16_ora.py
+++ 15_16_ora.py
@@ -6,13 +6,14 @@ print('Hello')
 a = 'Hello'
 
 print(a)
-a =Lorem ipsum dolor sit amet,
+
+a = """Lorem ipsum dolor sit amet,
 
 consectetur adipiscing elit,
 
 sed do eiusmod tempor incididunt
 
-ut labore et dolore magna aliqua.
+ut labore et dolore magna aliqua."""
 
 print(a)
 
@@ -29,7 +30,7 @@ for x in "banana":
 for x in "banana":
 
   print(x,end='')
-'''
+
 
 a = "Hello, World!"
 
@@ -38,3 +39,66 @@ for x in a:
     if szamlalo % 2 == 0:
         print(a[szamlalo-1], end="")
      szamlalo = szamlalo + 1
+
+txt = "The best things in life are free!"
+
+print("free" in txt)
+
+
+txt = "The best things in life are free!"
+
+if "expensive" not in txt:
+
+  print("No, 'expensive' is NOT present.")
+
+
+b = "Hello, World!"
+
+print(b[2:7])
+
+
+b = "Hello, World!"
+
+print(b[1:5])
+
+c = "H"+b[1:5]
+print (c)
+
+
+b = "Hello, World!"
+
+print(b[2:]) # llo, World!
+
+
+a = "Hello, World!"
+
+print(a.upper())
+nagybetu = a.upper()
+print (nagybetu)
+
+
+a = "Hello, World!"
+
+print(a.lower())
+
+
+a = " Hello, World! "
+
+print(a.strip())
+
+a = "Jello, Jorld!"
+print (a)
+
+print(a.replace("J", "W"))
+'''
+
+a = "Hello, World!,2222,3333"
+
+print(a.split(",")) # returns ['Hello', ' World!']
+lista = a.split(",")
+print(lista[1])
+
+b = "44;341;223;333;6465"
+print(b.split(";"))
+lista1 = b.split(";")
+print(lista1)[3]
