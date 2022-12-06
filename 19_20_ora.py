914d3a922088ed38384334c7e978901977365d8d
baloght1 <112930774+baloght1@users.noreply.github.com>
2022-12-06 09:41:24 +0100

    Add files via upload

---
+++ 19_20_ora.py
@@ -0,0 +1,66 @@
+'''a = "hello"
+
+b = "world"
+
+c = a + " " + b
+
+print (c)
+
+.................................................................
+
+age = 36
+
+txt = "My name is John, I am " + age
+
+print(txt)
+
+.................................................................
+
+quantity = 3
+
+itemno = 567
+
+price = 49.95
+
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+
+print(myorder.format(quantity, itemno, price))
+
+print ("asdasdasd \"FSF\" \nuj sor\titt egy tabulator van")
+
+print("sdfomsdmp",end="\n\n\n")
+print("dőpfmdofdl",end="\t")
+print("dfsgdf")
+
+................................................................
+
+szam = 0
+while szam != 100:
+    szam = szam+1
+    print(szam)
+print("A prog. vege!")
+#1...100
+
+................................................................
+
+szam = 0
+while szam != 100:
+    print(szam)
+    szam = szam + 1
+print("A prog. vege!")
+#0...99
+
+
+szam = 1
+while szam != 101:
+    print(szam)
+    szam = szam + 1
+print("A prog. vege!")
+'''
+
+
+szam = 1
+while True:
+    print(szam)
+    szam = szam + 200
+print("A prog. vege!")
---
+++ 19_20_ora_hf.py
@@ -0,0 +1,18 @@
+'''
+szam = 1
+while szam != 100:
+    print(szam)
+    szam = szam + 2
+
+#2.F
+
+szam = 1
+while szam != 100:
+    print(szam)
+    szam = szam + 3
+'''
+#3.F
+szam = 1
+while szam <100:
+    print(szam)
+    if szam %2 == szam 0:
