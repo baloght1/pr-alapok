a0faf62c290ef97f19653c0ce9b999889f9c8bce
baloght1 <112930774+baloght1@users.noreply.github.com>
2023-03-08 07:45:02 +0100

    Add files via upload

---
+++ 03_08.py
@@ -0,0 +1,14 @@
+#Balogh Tivadar A 10.c 03_08
+
+
+szam_sorozat= [15,4,5,19,2]
+
+def megszamlalas ():
+    index = 0
+    x = 0
+    for szam in szam_sorozat:
+        if x < szam 
+        darab = darab + 1
+
+        print(f"a listaban {darab} szam megtalalhato")
+        megszamlalas()
